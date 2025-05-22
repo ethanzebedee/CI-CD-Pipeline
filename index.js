da38e8efacb26450c0a39b1b2dc92f3a852d6e0c
Ethan <gonedaft890@gmail.com>
2025-05-22 16:43:35 +0100

    reconfigured tests
    reconfigured tests to properly close server after tests

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+let server;
+if (require.main === module) {
+  server = app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+  });
+} else {
+  server = app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+  });
+}
 
-module.exports = app;
+module.exports = { app, server };
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,11 +1,15 @@
 const request = require("supertest");
-const app = require("../index");
-const { describe, it, expect } = require("@jest/globals");
+const { app, server } = require("../index");
+const { describe, it, expect, afterAll } = require("@jest/globals");
 
 describe("GET /", () => {
   it("should respond status 200 with Hello World!", async () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe("Hello World!");
+  });
+
+  afterAll((done) => {
+    server.close(done);
   });
 });
